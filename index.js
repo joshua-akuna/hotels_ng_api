b5f8236230ddf4778c9fac18b4ff6be3d7641c68
root <root@tarajack.>
2025-10-16 06:10:24 +0100

    connected to mongodb

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
+
 // initialize express app
 const app = express()
 const PORT = process.env.PORT || 5000
 
+//mongodb connection
+const connect = async()=> {
+    try {
+        mongoose.connect(process.env.MONGO_URI)
+    } catch (error) {
+        throw error;
+    }
+}
+
+// app routes
 app.get('/', (req, res)=> {
+    connect();
     res.json('Hello world');
 })
 
 app.listen(PORT, ()=>{
+    connect();
     console.log(`app listening on port ${PORT}`);
 })
